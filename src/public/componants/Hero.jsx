bf5aaa28d429ab1f0fee7ac1a755e5122073e7c7
vishal gote <vishal27@gmail.com>
2024-12-07 13:07:11 +0530

    ekkaindustires

--- src/public/componants/Hero.jsx
+++ src/public/componants/Hero.jsx
@@ -20,25 +20,25 @@ const Hero = () => {
             />
         </div> */}
 
+
+
+
         <div className="w-full mt-16">
             {/* Image for mobile view */}
             <img
                 src="/mobileview.jpg"  // Image for mobile screens
                 alt="Furniture Banner"
-                className="w-full h-screen sm:hidden object-cover"  // Visible on mobile only (hidden on sm and up)
+                className="w-full h-auto sm:hidden object-cover"  // Visible on mobile only (hidden on sm and up)
             />
 
             {/* Image for larger screens (sm, md, lg, xl) */}
-            {/* <img
-                src="/heroekka.png"  
-                alt="Furniture Banner"
-                className="hidden sm:block w-full h-52 sm:h-80 md:h-96 lg:h-[30rem] xl:h-screen object-cover"  // Visible on larger screens (sm and up)
-            /> */}
             <img
-                src="/heroekka.png"  // Image for larger screens
+                src="/heroekka.png"
                 alt="Furniture Banner"
-                className="hidden sm:block w-full h-52 sm:h-80 md:h-96 lg:h-screen xl:h-screen object-cover"  // Visible on larger screens (sm and up)
+                className="hidden sm:block w-full h-52 sm:h-80 md:h-[27rem] lg:h-[34rem] xl:h-[89vh] aspect-video object-cover"  // Visible on larger screens (sm and up)
             />
+
+
         </div>
 
 
